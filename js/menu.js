bf129a3c2fad5f2f565eb4675c2f6fed16560c14
jalilmanuel25 <jalilmanuel25@gmail.com>
2020-06-19 22:18:35 -0500

    Se guardan cambios

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,9 @@ $(document).ready(function(){
         if($(this).scrollTop() > 0){
             $('.menu').addClass('menu2');
             $('.contenedor-menu').addClass('contenedor-menu-2');
-            $("#img-menu").attr("src","img/fullwings.png");
         }else{
             $('.menu').removeClass('menu2');
             $('.contenedor-menu').removeClass('contenedor-menu-2');
-            $("#img-menu").attr("src","img/tragonauta.png");
         }
     });
 });
